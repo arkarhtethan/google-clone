01588b3787a6703a1df29168a39195f697977d4c
Kaung Myat Han <arkarhtethan@gmail.com>
2022-02-16 20:15:00 +0630

    pagination buttons added

---
+++ components/PaginationButtons.jsx
@@ -0,0 +1,31 @@
+import { useRouter } from 'next/router'
+import React from 'react'
+import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/solid'
+import Link from 'next/link'
+
+const PaginationButtons = () => {
+  const router = useRouter()
+  const startIndex = Number(router.query.start) || 0
+  return (
+    <div className="mb-10 flex max-w-lg justify-between text-blue-700">
+      {startIndex >= 10 && (
+        <Link
+          href={`/search?term=${router.query.term}&start=${startIndex - 10}`}
+        >
+          <div className="flex flex-grow cursor-pointer flex-col items-center hover:underline">
+            <ChevronLeftIcon className="h-5" />
+            <p>Previous</p>
+          </div>
+        </Link>
+      )}
+      <Link href={`/search?term=${router.query.term}&start=${startIndex + 10}`}>
+        <div className="flex flex-grow cursor-pointer flex-col items-center hover:underline">
+          <ChevronRightIcon className="h-5" />
+          <p>Next</p>
+        </div>
+      </Link>
+    </div>
+  )
+}
+
+export default PaginationButtons
